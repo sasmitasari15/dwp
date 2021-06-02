8219c1bfdba99e308e2187ebae4d41654a6be62e
Sasmita Sari <82150207+sasmitasari15@users.noreply.github.com>
2021-06-02 23:53:13 +0700

    Add files via upload

---
+++ 6/filejs.js
@@ -0,0 +1,16 @@
+while(!nama){
+	var nama = prompt("masukkan nama kamu:").toUpperCase();
+};
+while(!pacar){
+var pacar = prompt("masukkan nama pacar kamu:").toUpperCase();
+};
+document.getElementById("txtnama").value = nama;
+document.getElementById("txtpacar").value = pacar;
+alert(nama + " pacar " + pacar);
+var konfirmasi  = confirm("apakah " + pacar + " mencintai " + nama + "?");
+if (konfirmasi === true) {
+document.getElementById("hasil").innerHTML = "semoga " + nama + " & " + pacar + " bahagia selamanya"
+;
+} else {
+document.getElementById("hasil").innerHTML = "asik, rebut " + pacar + " ah dari " + nama;
+}
